d2b56478ba3a2fd080805b8fa2427f2c163013c4
Jesús Velaztiqui <144689350+jesusvelaz619@users.noreply.github.com>
2025-06-19 17:06:46 -0300

    S

--- src/pages/Question/Question.jsx
+++ src/pages/Question/Question.jsx
@@ -5,27 +5,60 @@ import { useNavigate } from "react-router-dom";
 const Question = () => {
   const [clickedIndex, setClickedIndex] = useState(null);
   const [pregunta, setPregunta] = useState(null);
+  const [categoria, setcategoria] = useState("");
+  const [progreso, setProgreso] = useState(0);
   const navigate = useNavigate();
   useEffect(() => {
-    const categoria = localStorage.getItem("selectedCategory");
+    setcategoria(localStorage.getItem("selectedCategory"));
     const categoriaSeleccionada = questions.filter((q) => q.tipo === categoria);
     const randomIndex = Math.floor(
       Math.random() * categoriaSeleccionada[0]?.questions?.length
     );
     const preguntaAleatoria = categoriaSeleccionada[0]?.questions[randomIndex];
     setPregunta(preguntaAleatoria);
+
+    const interval = setInterval(() => {
+      setProgreso((prev) => {
+        if (prev >= 100) {
+          clearInterval(interval);
+          return 100;
+        }
+        return prev + 2;
+      });
+    }, 600);
+    return () => clearInterval(interval);
+  }, [categoria]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgreso((prev) => {
+        if (prev >= 100) {
+          clearInterval(interval);
+          return 100;
+        }
+        return prev + 2;
+      });
+    }, 100);
+    return () => clearInterval(interval);
   }, []);
 
   const handleQuestions = (index) => {
     setClickedIndex(index);
-
     setTimeout(() => {
       navigate("/");
     }, 1000);
   };
+  useEffect(() => {
+    if (progreso >= 100 && pregunta) {
+      navigate("/");
+    }
+  }, [progreso]);
 
   return (
     <div className="body">
+      <div className="divProgreso">
+        <div className="progreso" style={{ width: `${progreso}%` }}></div>
+      </div>
       <div className="quiz-container">
         <div className="decorative-elements">
           <div className="bubble"></div>
@@ -35,7 +68,9 @@ const Question = () => {
         </div>
 
         <div className="quiz-header">
-          <h1 className="quiz-title">Pon a Prueba tu Conocimiento</h1>
+          <h1 className="quiz-title">
+            Pon a Prueba tu Conocimiento en ( {categoria} )
+          </h1>
         </div>
 
         <div className="question-card">
